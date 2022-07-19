b0091320f00c4d90e54dac22f2b096749072885b
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-19 17:23:48 -0600

    Excersize 1

---
+++ FactoryPatternExcersize1/Program.cs
@@ -0,0 +1,7 @@
+﻿using FactoryPatternExcersize1;
+
+Console.WriteLine("Type in the number of tires you want on your vehicle");
+var userVehicle = Convert.ToInt32(Console.ReadLine());
+
+var vehicle =  VehicleFactory.GetVehicle(userVehicle);
+vehicle.Drive();
---
+++ FactoryPatternExcersize1/VehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExcersize1
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int tires)
+        {
+            switch (tires)
+            {
+                case 2:
+                    return new Motorcycle();
+                case 4:
+                    return new Car();
+                default:
+                    return new Car();
+            }
+        }
+
+       
+    }
+}
